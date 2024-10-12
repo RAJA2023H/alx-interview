0e40881b735923221a9d33a033c05dbf2d194d9d
rajae <Hemmanyrajae@gmail.com>
2024-10-11 23:05:19 -0700

    PEP8 validated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,14 +2,16 @@
 """
 defines method that determines if all the boxes can be opened.
 """
+
+
 def canUnlockAll(boxes):
     """
     doc
     """
 
     n = len(boxes)
-    opened = set([0]) #Set of opened boxes, data structure"""
-    stack = [0] #initialize the stack with [0]"""
+    opened = set([0])  # Set of opened boxes, data structure"""
+    stack = [0]  # initialize the stack with [0]"""
 
     while stack:
         current_box = stack.pop()
@@ -17,4 +19,4 @@ def canUnlockAll(boxes):
             if key < n and key not in opened:
                 opened.add(key)
                 stack.append(key)
-    return len(opened) == n #debug"""
+    return len(opened) == n  # debug"""
