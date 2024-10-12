f9f29da7c79f4c8ebda468970f358c6168195d2f
rajae <Hemmanyrajae@gmail.com>
2024-10-11 22:43:53 -0700

    first solution for locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+defines method that determines if all the boxes can be opened.
+"""
+def canUnlockAll(boxes):
+    """
+    doc
+    """
+
+    n = len(boxes)
+    opened = set([0]) #Set of opened boxes, data structure"""
+    stack = [0] #initialize the stack with [0]"""
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key < n and key not in opened:
+                opened.add(key)
+                stack.append(key)
+    return len(opened) == n #debug"""
